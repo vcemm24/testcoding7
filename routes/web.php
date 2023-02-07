7240031379734dd2ae521720248d5c3a34fe9346
vcemm24 <vcemm24@gmail.com>
2023-02-07 10:11:38 +0100

    creating index page...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings.index',[
+        'listings'=>Listing::all()
+        //'listings'=>''
+    ]);
 });
